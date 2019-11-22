0d03f1c2dfc35780aa5a1f6802bd099cde45ee2e
hoangphuong <phamdophuonghoang@gmail.com>
2019-11-22 14:39:07 +0000

    phuong

---
+++ ip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! [ $# -eq 1 ]; then
+      echo "provice one ag"
+      exit 1
+fi
+VAR=$1
+REGEX=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}
+ if ! [[ $VAR =~ $REGEX ]]; then
+      echo " REGEX no exit"
+      exit 2
+fi 
+IP=${BASH_REMATCH[0]}
+ping -c1 $IP 1>/dev/null
+if [ $? -eq 0 ]; then
+    echo " TRUE "
+else
+   echo "FAIL"
+fi
